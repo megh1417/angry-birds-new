d20febfeb74d39d8b0ac771ee7506539247d7214
megh1417 <73287255+megh1417@users.noreply.github.com>
2021-01-05 17:56:41 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -34,11 +34,11 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,50);
 
   
 
-   slingshot= new Slingshot(bird.body,{x:200 , y:100})
+   slingshot= new Slingshot(bird.body,{x:200 , y:50})
 }
 
 function draw(){
